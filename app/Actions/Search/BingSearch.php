61c7e5ba7167fc33ce23bd340c46795d311512f7
Edmario Oliveira <barrigacraft@hotmail.com>
2024-10-04 21:42:13 -0300

    teste consulta

--- app/Actions/Search/BingSearch.php
+++ app/Actions/Search/BingSearch.php
@@ -14,20 +14,16 @@ class BingSearch
     public function __construct() {}
 
     /**
-     * 
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Support\Collection<PdfDto>
+     * @return \Illuminate\Support\Collection
      */
-    public function execute(Request $request)
+    public function execute(Request $request): Collection
     {
-        /**
-         * @var Collection<PdfDto>
-         */
         $pdfs = collect();
         $paginations = [1, 50, 100];
-
+    
         foreach ($paginations as $first) {
-            $search = urlencode('"'.trim($request->input('s')).'"'. " filetype:pdf (site:amazonaws.com OR site:archive.org) -related:baidu.com");
+            $search = urlencode(trim($request->input('s')) . " filetype:pdf");
             $url = "https://www.bing.com/search?q=" . $search . "&count=50&first=$first&setlang=pt-BR&cc=BR";
             $web = new \Spekulatius\PHPScraper\PHPScraper;
             $web->setConfig([
@@ -35,17 +31,17 @@ class BingSearch
                 'agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
             ]);
             $web->go($url);
-            $list = ($web->filter("//li[@class='b_algo']")->each(function ($node) {
-                $dto = new PdfDto([
+            $list = $web->filter("//li[@class='b_algo']")->each(function ($node) {
+                return new PdfDto([
                     'link' => $node->filter("h2")->filter('a')->attr("href"),
                     'title' => $node->filter("h2")->filter('a')->text(),
                     'description' => substr($node->filter("div")->filter("p")->text(), 3)
                 ]);
-                return $dto;
-            }));
+            });
+    
             $pdfs->push(...$list);
         }
-
-        return collect($pdfs);
+        return $pdfs;
     }
+    
 }
